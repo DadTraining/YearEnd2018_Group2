b10f923927b39f36afea34e4aaaad83838093daf
Nhu Le <nhulemis@gmail.com>
2019-01-28 08:41:27 +0700

    add model

--- projectgame/proj.win32/Model.h
+++ projectgame/proj.win32/Model.h
@@ -1,11 +1,17 @@
 #pragma once
+#include "cocos2d.h"
 class Model
 {
 private:
-
+	cocos2d::Sprite* mSprite;
+	cocos2d::Vec2* mPos;
+	bool mVisible;
 public:
 	Model();
  	virtual ~Model();
+
+	void SetVisible(bool vis);
+	bool IsVisible();
 
 	virtual void Update() = 0;
 	virtual void Init() = 0;
