27a669e65fd081acb385836299ea3bbacac9a574
Vinhhung124 <vinhhung120497@gmail.com>
2019-01-30 15:23:00 +0700

    define edit

--- projectgame/Classes/define.h
+++ projectgame/Classes/define.h
@@ -1,2 +1,12 @@
 #pragma once
-//
+#define SCREEN_W 1024
+#define SCREEN_H 576
+#define FPS 40
+
+#define SHIP_IMG "Ship.png"
+#define SHIP_SCALE 0.12
+#define SHIP_SPEED 2
+
+
+#define BULLET_IMG "Bullet.png"
+#define BULLET_SPEED 3
