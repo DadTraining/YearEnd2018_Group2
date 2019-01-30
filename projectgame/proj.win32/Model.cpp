de3cf2c7e7b65da846f8114a9bc94aa27e8bd6df
Vinhhung124 <vinhhung120497@gmail.com>
2019-01-30 15:22:25 +0700

    edit model class

--- projectgame/proj.win32/Model.cpp
+++ projectgame/proj.win32/Model.cpp
@@ -18,3 +18,26 @@ bool Model::IsVisible()
 {
 	return mVisible;
 }
+
+
+cocos2d::Vec2 Model::GetPosition()
+{
+	return this->GetSprite()->getPosition();
+}
+
+void Model::SetPosition(cocos2d::Vec2 mPos)
+{
+	this->GetSprite()->setPosition(mPos);
+}
+cocos2d::Sprite * Model::GetSprite()
+{
+	return this->mSprite;
+}
+
+void Model::SetSprite(cocos2d::Sprite * sprite)
+{
+	this->mSprite = sprite;
+}
+cocos2d::Vec2 Model::GetLocation() {
+	return mSprite->getPosition();
+}
