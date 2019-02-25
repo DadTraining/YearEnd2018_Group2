7197b1cf864ee3bfe580745017d7de0939ed0be0
Nhu Le <nhulemis@gmail.com>
2019-02-25 11:48:38 +0700

    infomap class

---
+++ projectgame/Classes/InfoMap.cpp
@@ -0,0 +1,20 @@
+#include "InfoMap.h"
+
+int InfoMap::SHARK_PHASE_1ST = 0;
+int InfoMap::SHARK_PHASE_2ND = 0;
+int InfoMap::SHARK_PHASE_3RD = 0;
+
+int InfoMap::getPhase1()
+{
+	return SHARK_PHASE_1ST;
+}
+
+int InfoMap::getPhase2()
+{
+	return SHARK_PHASE_2ND;
+}
+
+int InfoMap::getPhase3()
+{
+	return SHARK_PHASE_3RD;
+}
